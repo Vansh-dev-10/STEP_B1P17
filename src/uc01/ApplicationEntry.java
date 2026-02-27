bfa952b1d90415ad2069fd041c1e63f14541932f
adx69 <ar6553@gmail.com>
2026-02-27 09:09:20 +0530

    UC01 - Application Entry & Welcome Message

---
+++ src/uc01/ApplicationEntry.java
@@ -0,0 +1,17 @@
+package uc01;
+
+public class ApplicationEntry {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("   Welcome to Palindrome Checker App  ");
+        System.out.println("======================================");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+        System.out.println("Developer        : Vansh");
+        System.out.println("======================================");
+
+        System.out.println("Application started successfully...");
+    }
+}
